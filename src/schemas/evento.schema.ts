bbd84dde7e0a8c32f570f45231d7ed5b1129aeab
MarcoBernaus <marcobernaus04@gmail.com>
2026-06-22 11:10:12 -0300

    crud evento, service y schema realizados, sin testear

---
+++ src/schemas/evento.schema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const create_evento_schema = z.object({
+  
+  titulo: z.string().min(1),
+  categoria: z.string().min(1),
+  stock: z.number().int(),
+  fecha_hora_inicio: z.string().datetime(),
+  fecha_hora_fin: z.string().datetime(),
+  estado: z.enum(['PENDIENTE', 'CONFIRMADO', 'REALIZADO', 'CANCELADO']),
+  id_organizador: z.number().int().positive(),
+  id_lugar: z.number().int().positive(),
+
+  });
+  
+export const update_evento_schema = create_evento_schema.partial();
+
+export type create_evento_DTO = z.infer<typeof create_evento_schema>;
+export type update_evento_DTO = z.infer<typeof update_evento_schema>;
---
+++ src/services/evento.service.ts
@@ -0,0 +1,88 @@
+import prisma from '../config/prisma'
+import { estado_evento } from '../generated/prisma/index.js';
+import { create_evento_DTO, update_evento_DTO } from '../schemas/evento.schema';
+
+
+export const create = async (data: create_evento_DTO) => {
+
+  try{
+    const organizador = await prisma.organizador.findUnique({
+      where: { id: data.id_organizador }
+    });
+    if (!organizador) throw new Error('El organizador no existe');
+
+
+    const lugar = await prisma.lugar.findUnique({
+      where: { id: data.id_lugar }
+    });
+    if (!lugar) throw new Error('El lugar no existe');
+
+
+    
+        const evento_creado = await prisma.evento.create({ data });
+        return evento_creado;
+    
+  } catch(error){
+      throw error;
+  }
+};
+
+
+
+export const update = async (id: number, data: update_evento_DTO) => {
+  try{
+      if (data.id_organizador){
+        const organizador = await prisma.organizador.findUnique({
+          where: { id: data.id_organizador }
+      }); 
+      if (!organizador) throw new Error('El organizador no existe');
+    }
+
+      if (data.id_lugar) {
+        const lugar = await prisma.lugar.findUnique({
+        where: { id: data.id_lugar }
+        });
+      if (!lugar) throw new Error('El lugar no existe');
+      }
+        const evento_actualizado = await prisma.evento.update({where: { id }, data });
+        return evento_actualizado;
+    
+  } catch(error){
+      throw error;
+  }
+};
+  
+  
+
+
+export const delete_ = async (id: number, data: update_evento_DTO) => {
+  try{  
+  if (data.id_organizador){
+        const organizador = await prisma.organizador.findUnique({
+          where: { id: data.id_organizador }
+      }); 
+      if (!organizador) throw new Error('El organizador no existe');
+    }
+
+      if (data.id_lugar) {
+        const lugar = await prisma.lugar.findUnique({
+        where: { id: data.id_lugar }
+        });
+      if (!lugar) throw new Error('El lugar no existe');
+      }
+        const evento_cancelado = await prisma.evento.update({where: { id }, data:{estado:'CANCELADO'} });
+        return evento_cancelado;
+    
+  }catch(error){
+      throw error;
+  }
+    };
+
+    
+    
+    
+    
+    
+    
+    
+    
