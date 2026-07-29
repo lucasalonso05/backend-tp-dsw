6e1a40144e7a61bd8b74b56f0d1d3d16945534c6
MarcoBernaus <marcobernaus04@gmail.com>
2026-07-29 15:03:54 -0300

    modificacion de index.ts por routers de cruds modificados al ingles al momento

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 import { config } from './config/config';
 import logger from './config/logger';
-import lugarRouter from './routes/place.routes';
+import placeRouter from './routes/place.routes';
 import organizadorRouter from './routes/organizador.routes'
 import assistantRouter from './routes/assistant.routes'
-import compradorRouter from './routes/buyer.routes'
-import eventoRouter from './routes/event.routes'
+import buyerRouter from './routes/buyer.routes'
+import eventRouter from './routes/event.routes'
 import ordenRouter from './routes/orden.routes'
-import entradaRouter from './routes/entry.routes'
+import entryRouter from './routes/entry.routes'
 import ticketdigitalRouter from './routes/ticket_digital.routes'
 
 const app: Application = express();
@@ -50,18 +50,18 @@ app.listen(config.port, () => {
 });
 
 
-app.use('/lugares', lugarRouter);
+app.use('/lugares', placeRouter);
 
 app.use('/organizadores', organizadorRouter);
 
 app.use('/asistentes', assistantRouter);
 
-app.use('/compradores', compradorRouter);
+app.use('/compradores', buyerRouter);
 
-app.use('/eventos', eventoRouter);
+app.use('/eventos', eventRouter);
 
 app.use('/ordenes', ordenRouter);
 
-app.use('/entradas', entradaRouter)
+app.use('/entradas', entryRouter)
 
 app.use('/ticketsdigital', ticketdigitalRouter)
